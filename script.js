2bc157f3b0f8f7ff9ad8cff0a2ab697b562a44d7
Dian Rifan Abdulah <iyanriv4@gmail.com>
2026-06-20 08:01:39 +0700

    Initial calculator project

---
+++ script.js
@@ -0,0 +1,19 @@
+function tambah() {
+    // Anggota 1
+}
+
+function kurang() {
+    // Anggota 2
+}
+
+function kali() {
+    // Anggota 3
+}
+
+function bagi() {
+    // Anggota 4
+}
+
+function clearData() {
+    // Anggota 5
+}
